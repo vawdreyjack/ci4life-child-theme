0dc110c199ad8110b21f647979f717203e8ca6c4
Jack Vawdrey <vawdreyjack@gmail.com>
2020-07-09 08:33:59 -0600

    hiding the admin bar for everyone except admins

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+//Hide the admin bar for everyone except admins
+add_action('after_setup_theme', 'remove_admin_bar');
+ 
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
